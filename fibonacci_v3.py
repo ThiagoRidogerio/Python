31efe004857567d26ca1966bf350feacaef6b7c7
Thiago Ridogério <65817575+ThiagoRidogerio@users.noreply.github.com>
2022-05-03 13:18:19 -0300

    commit inicial

---
+++ fibonacci_v3.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+# Sequência Fibonacci com quantidade de interações limitadas
+
+def fibonacci(quantidade):
+    resultado = [0, 1]
+    while True:
+        resultado.append(sum(resultado[-2:]))
+        if len(resultado) == quantidade:
+            break
+    return resultado
+
+
+if __name__ == '__main__':
+    # listar os 20 primeiros números da sequência
+    for fib in fibonacci(20):
+        print(fib)
